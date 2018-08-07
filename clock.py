54d3b829e8a78d562120b21a81e07de547966bd9
Rich Stillman <stillmanff@users.noreply.github.com>
2018-08-06 23:06:42 -0400

    Update clock.py
    Modified clock to work like I wanted it to...

--- clock.py
+++ clock.py
@@ -2,6 +2,7 @@
 
 from sense_hat import SenseHat
 import time
+from time import sleep
 
 sense = SenseHat()
 
@@ -48,10 +49,21 @@ number = [
 0,0,0,1
 ]
 
+blank = [
+0,0,0,0,
+0,0,0,0,
+0,0,0,0,
+0,0,0,0
+]
+
+
+twelvehour = True
+blinkingSecond = True
+
 hourColour   = [255,0,0] # red
 minuteColour = [0,255,255] # cyan
 empty        = [0,0,0] # off / black
-
+white        = [255,255,255] # white
 clockImage = [
 0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,
@@ -63,37 +75,73 @@ clockImage = [
 0,0,0,0,0,0,0,0
 ]
 
-hour = time.localtime().tm_hour
-minute = time.localtime().tm_min
+if twelvehour:
+        print '12 hour mode'
+else:
+        print '24 hour mode'
+if blinkingSecond:
+        print 'Blinking seconds enabled'
+else:
+        print 'Blinking seconds disabled'
+                
 
-# Map digits to clockImage array
+while True:
+	hour = time.localtime().tm_hour
+        if twelvehour:
+                if hour > 12:
+                        hour = hour - 12
+                if hour == 0:
+                        hour = 12
+#        hour = hour + 2     #Debug adjustment - remove this
+	minute = time.localtime().tm_min
 
-pixelOffset = 0
-index = 0
-for indexLoop in range(0, 4):
-	for counterLoop in range(0, 4):
-		if (hour >= 10):
-			clockImage[index] = number[int(hour/10)*16+pixelOffset]
-		clockImage[index+4] = number[int(hour%10)*16+pixelOffset]
-		clockImage[index+32] = number[int(minute/10)*16+pixelOffset]
-		clockImage[index+36] = number[int(minute%10)*16+pixelOffset]
-		pixelOffset = pixelOffset + 1
-		index = index + 1
-	index = index + 4
+	# Map digits to clockImage array
 
-# Colour the hours and minutes
+	pixelOffset = 0
+	index = 0
+	for indexLoop in range(0, 4):
+		for counterLoop in range(0, 4):
+			if (hour >= 10):
+				clockImage[index] = number[int(hour/10)*16+pixelOffset]
+                        else:
+                                clockImage[index] = blank
+			clockImage[index+4] = number[int(hour%10)*16+pixelOffset]
+			clockImage[index+32] = number[int(minute/10)*16+pixelOffset]
+			clockImage[index+36] = number[int(minute%10)*16+pixelOffset]
+			pixelOffset = pixelOffset + 1
+			index = index + 1
+		index = index + 4
 
-for index in range(0, 64):
-	if (clockImage[index]):
-		if index < 32:
-			clockImage[index] = hourColour
+	# Colour the hours and minutes
+
+	for index in range(0, 64):
+		if (clockImage[index]):
+                        if index < 32:
+				clockImage[index] = hourColour
+			else:
+				clockImage[index] = minuteColour
 		else:
-			clockImage[index] = minuteColour
-	else:
-		clockImage[index] = empty
+			clockImage[index] = empty
 
-# Display the time
+        # Clear the leading hour if zero
+        if (hour < 10):
+                for outerIndex in range (0,25,8):
+                        for indexLoop in range (0,4):
+                                clockImage[indexLoop + outerIndex] = empty
+                        
 
-sense.set_rotation(90) # Optional
-sense.low_light = True # Optional
-sense.set_pixels(clockImage)
+	# Display the time
+
+	sense.set_rotation(180) # Optional
+	sense.low_light = True # Optional
+        if blinkingSecond:
+                clockImage[0] = white
+                sense.set_pixels(clockImage)
+                sleep(1)
+                clockImage[0] = empty
+                sense.set_pixels(clockImage)
+                sleep(1)
+        else:
+                sense.set_pixels(clockImage)
+                sleep(2)
+        
